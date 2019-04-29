ec4ff485057915b70f9e0be49730855f4861cd3f
langlang1010 <3225910491@qq.com>
2019-04-29 19:26:14 +0800

    修改了一些重要内容

--- freeswitch/README.md
+++ freeswitch/README.md
@@ -5,6 +5,10 @@
 
 ​		配置路径： /etc/freeswitch/diaplan/default.xml
 
+​		作用：拨号计划，配置用户拨打号码与服务内容之间的映射。
+
 ### 2. lua
 
 ​		配置路径：/usr/share/freeswitch/scripts
+
+​		作用：用来处理用户的按键、语音、留言等消息。
--- freeswitch/scripts/test.lua
+++ freeswitch/scripts/test.lua
@@ -1,14 +1,26 @@
 --- 用来记录当前进入的状态
 grade_now = 1
+sound_now = 0
 function onInput(s,type,obj,arg)
     if (type == "dtmf")  then
         freeswitch.consoleLog("INFO", "DTMF: ".. obj.digit .. "Duration:" .. obj.duration .. "\n")
         if (obj.digit == "0") then
            -- actionVoice(1)
+		-- 返回上一级
         elseif (obj.digit == "#") then
-                        grade_now = 1
+            grade_now = 1
             session:streamFile("/usr/share/freeswitch/sounds/index.wav")
-                        session:streamFile("silence_stream://-1,1400") 
+            session:streamFile("silence_stream://-1,1400") 
+		elseif (obj.digit == "*") then
+           if(sound_now == 0) then
+		       
+		       session:streamFile("/usr/share/freeswitch/sounds/index.wav")
+               session:streamFile("silence_stream://-1,1400") 
+		   else
+              	   
+               session:streamFile("/usr/share/freeswitch/sounds/g"..sound_now..".wav")
+               session:streamFile("silence_stream://-1,1400") 
+           end
         else 
            actionVoice(obj.digit)
         end	
@@ -18,11 +30,12 @@ function onInput(s,type,obj,arg)
 end
 
 function actionVoice(which)
-        if (grade_now == 1) then 
-                      grade_now = 2
-                session:streamFile("/usr/share/freeswitch/sounds/g"..which..".wav")
-                session:streamFile("silence_stream://-1,1400") 
-        end
+    if (grade_now == 1) then 
+        grade_now = 2
+        sound_now = which
+        session:streamFile("/usr/share/freeswitch/sounds/g"..which..".wav")
+        session:streamFile("silence_stream://-1,1400") 
+    end
 end 
 session:answer()
 session:setInputCallback('onInput','')
