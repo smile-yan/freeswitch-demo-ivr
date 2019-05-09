f5c015b8558fc16cc6d7d6c82fe5e79da8c33048
smile-yan <root@smileyan.cn>
2019-05-09 12:07:43 +0800

    update readme

--- freeswitch/scripts/test.lua
+++ freeswitch/scripts/test.lua
@@ -18,11 +18,29 @@ function onInput(s,type,obj,arg)
 			if(sound_now=="2") then 
 				session:speak("请您说出您希望了解的学院名称.......");
 				local result = getARSResult()
-				session:speak("您所说的是"..result)
+				if(result==nil) then
+					session:speak("不能听清您所说的，请再说一遍.......");
+				else
+					-- 去掉空格 句号  >  "学院"
+					local s1 = string.gsub(result, "。", "") 
+					local s2 = string.gsub(s1, " ", "")
+   					local s3 = string.gsub(s2, ">", "") 
+					local s4 = string.gsub(s3, "学院", "") 
+
+					local w = getAcademy(s4)
+					if(w==nil) then
+						session:speak("您所说的是"..result.."但是并不清楚是什么学院")
+					else 
+						session:speak("您所说的是"..result.."编号为："..w)
+						print(w.."\n")
+					end
+				end
+				
 			else
 				if(sound_now == "4" or sound_now=="5") then
 					session:speak("请您说出您希望了解的专业.......");
 					local result = getARSResult()
+					
 					session:speak("您所说的是"..result)
 				end
 			end
@@ -88,6 +106,32 @@ function getValue(tempxml,key)
 	local tag2 = "</"..key..">"
 	_,_,value=string.find(tempxml, tag1.."(.-)"..tag2 )
 	return value
+end
+
+-- 所有学院
+academies = {"资源环境与安全工程学院","土木工程学院","机电工程学院","信息与电气工程学院","计算机科学与工程学院","化学化工学院","数学与计算科学学院","物理与电子科学学院","生命科学学院","建筑与艺术设计学院","人文学院","外国语学院","马克思主义学院","教育学院","商学院","艺术学院","体育学院","法学与公共管理学院","材料科学与工程学院","潇湘学院"}
+
+-- 返回学院编号
+function getAcademy(speech)
+	-- "数学学院" "**数学**" 
+	if(string.find(speech,"数学")) then 
+		return 7
+	end
+
+	-- "计算机学院"  "**计算机**"
+	if(string.find(speech,"计算机")) then 
+		return 5
+	end
+
+	
+	for i= 1,20 do
+		if(string.find(academies[i],speech.."") == nil) then
+			
+		else
+			return i
+		end
+	end
+	return nil
 end
 
 
